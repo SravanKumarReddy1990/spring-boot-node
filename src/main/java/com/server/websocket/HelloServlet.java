4491dda73df9265dc329a7b38a9127036df15c57
sravan <bskreddy1990@gmail.com>
2019-04-22 21:09:03 +0530

    make it better

--- src/main/java/com/server/websocket/HelloServlet.java
+++ src/main/java/com/server/websocket/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.simple.JSONObject; 
+import org.json.JSONObject;
 @WebServlet(
         name = "MyServlet",
         urlPatterns = {"/hello"}
