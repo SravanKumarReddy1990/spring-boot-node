9a6ff9415caeebecbdfee12c1b7a85e3a9b17e3e
sravan <bskreddy1990@gmail.com>
2019-09-26 21:08:11 +0530

    make it better

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,3 +1,11 @@
 $(function () {
   $('body p').append(" and Javascript!");
 });
+module.exports = function(grunt) {
+
+  // A very basic default task.
+  grunt.registerTask('default', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...').ok();
+  });
+
+};
