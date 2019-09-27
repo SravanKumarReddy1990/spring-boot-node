11969d3c973ff1f72fe8451a8c879263fd40c84c
sravan <bskreddy1990@gmail.com>
2019-09-27 11:10:52 +0530

    Initial release

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,11 +1,3 @@
 $(function () {
   $('body p').append(" and Javascript!");
 });
-module.exports = function(grunt) {
-
-  // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
-
-};
