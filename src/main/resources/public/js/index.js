7518564328d59907ee66dba4fe208a5eb003502b
sravan <bskreddy1990@gmail.com>
2019-09-27 11:30:05 +0530

    Initial release

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,3 +1,15 @@
 $(function () {
   $('body p').append(" and Javascript!");
+var Addition=require('./Addition.js');
+console.log(Addition.AddNumber(1,2));
+
+var localTutor=require('./NodeTutorial.js');
+localTutor.NodeTutorial();
+//localTutor.NodeTutorial.pTutor();
+ $('body p').append(localTutor.NodeTutorial());
+var tut = new localTutor.NodeTutorial();  
+tut.pTutor();
+
+ $('body p').append(tut.pTutor());
+
 });
